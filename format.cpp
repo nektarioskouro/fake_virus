827913375349ea3c4dad10621fc65b7200141000
nektarioskouro <54715648+nektarioskouro@users.noreply.github.com>
2020-02-08 12:48:39 +0200

    Add files via upload

---
+++ format.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+   cout << "*****************************************************" << endl;
+   cout << "*                                                   *" << endl;   
+   cout << "*                                                   *" << endl;
+   cout << "*                                                   *" << endl;
+   cout << "*                                                   *" << endl;
+   cout << "*          #       #      #                         *" << endl;
+   cout << "*           #      #     #                          *" << endl; 
+   cout << "*            #     #    #                           *" << endl;
+   cout << "*             #    #   #                            *" << endl; 
+   cout << "*              #   #  #                             *" << endl;
+   cout << "*               #  # #                              *" << endl;
+   cout << "*       #######################                     *" << endl;  
+   cout << "*               # # #                               *" << endl;
+   cout << "*              #  #  #                              *" << endl;
+   cout << "*             #   #   #                             *" << endl;    
+   cout << "*            #    #    #                            *" << endl;  
+   cout << "*           #     #     #                           *" << endl;
+   cout << "*          #      #      #                          *" << endl;  
+   cout << "*                 #                                 *" << endl;  
+   cout << "*                                                   *" << endl;
+   cout << "*                                                   *" << endl; 
+   cout << "*                                                   *" << endl;
+   cout << "*                                                   *" << endl;   
+   cout << "*                                                   *" << endl;
+   cout << "*****************************************************" << endl;
+   
+   std::cout << "Press enter to start......." << endl;
+   std::cin.ignore();
+   
+   std::cout << "Deleting your photos.........." << endl;
+   std::cin.ignore();
+   cout << endl;
+   
+   std::cout << "Deleting your videos........." << endl;
+   std::cin.ignore();
+   cout << endl;
+   
+   std::cout << "Deleting your apps/softwares......." << endl;
+   std::cin.ignore();
+   cout << endl;
+   
+   std::cout << "Deleting your data........." << endl;
+   std::cin.ignore();
+   cout << endl;
+   
+   std::cout << "Deleting your folders......." << endl;
+   std::cin.ignore();
+   cout << endl;
+   
+   std::cout << "Removing your accounts......." << endl;
+   std::cin.ignore();
+   cout << endl;
+   
+   std::cout << "Installing this software........" << endl;
+   std::cin.ignore();
+   cout << endl;
+   
+   std::cout << "You're all set. Just restart your system :)" << endl;
+   return 0;
+}
+   
+                   
